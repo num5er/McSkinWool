883a0c7bb3c0a966d07580dc59861a1bec68b5a4
num5er <gameingboss2.0@gmail.com>
2025-11-27 21:55:54 -0800

    Added crafting recipe for tan_wool_block. Removed test item.

--- src/main/java/com/num5er/tanwool/init/ModItems.java
+++ src/main/java/com/num5er/tanwool/init/ModItems.java
@@ -10,5 +10,5 @@ public class ModItems
 {
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
-    public static final Item TAN_WOOL = new ItemBase("tan_wool_item");
+    //public static final Item TAN_WOOL = new ItemBase("tan_wool_item");
 }
--- src/main/java/com/num5er/tanwool/util/Reference.java
+++ src/main/java/com/num5er/tanwool/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "tanwool";
     public static final String MOD_NAME = "Tan Wool";
-    public static final String MOD_VERSION = "1.0";
+    public static final String MOD_VERSION = "1.1";
     public static final String MOD_AUTHOR = "num5er";
     public static final String MOD_URL = "https://github.com/num5er/McSkinWool";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
